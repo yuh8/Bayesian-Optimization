27894f5b482cab6af89b6db8a084f2aafe11a7be
Hongyang Yu <hongyangyu@MacBook-Pro.local>
2018-04-17 20:28:34 +1200

    Start to write function

--- bo_core/Bayesian_optimizer.py
+++ bo_core/Bayesian_optimizer.py
@@ -1,2 +1,19 @@
 import numpy as np
 import scipy as sp
+
+
+class fitGP:
+    '''
+    Make sure that the columns before the last column
+    contain the input data x and the last column
+    contains the output y
+    '''
+
+    def __init__(self, X):
+        '''
+        for now the only input parameter is X
+        '''
+        self.X = X
+
+    def standardize(self):
+        _meanX = np.mean(X,)
