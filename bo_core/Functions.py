fixed numerical issue with log_det of large matrix

bo_core/Functions.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-__all__ = ['demean', 'covSE', 'choleInvKs', 'Predict']
+__all__ = ['demean', 'covSE', 'choleInvKs','logdetX']
 
 
 def demean(X, std=1):
     meanX = np.mean(X, axis=0)
     stdX = np.std(X, axis=0)
     N = X.shape[0]
-    temp = np.tile(meanX, (N, 1))
+    if len(X.shape) == 1:
+        temp = meanX
+    else:
+        temp = np.tile(meanX, (N, 1))
     if std == 1:
         X -= temp
         X /= stdX
@@ -67,17 +70,7 @@ def choleInvKs(par, X, covFcn):
     return K, Ks, invKs
 
 
-def Predict(par, X, y, meany, Xpre, covFcn):
-    # Eq2.25 and 2.26 of RW book
-    Npre = Xpre.shape[0]
-    _, _, invKs = choleInvKs(par, X, covFcn)
-    kpre1 = covFcn(par, X, Xpre)
-    # Eq2.25
-    mean_Ypre = np.dot(np.dot(kpre1.T, invKs), y)
-    temp = np.tile(meany, (Npre, 1))
-    mean_Ypre += temp
-    # Eq2.26
-
-    var_Ypre = par[0]**2 - np.dot(np.dot(kpre1.T, invKs), kpre1)
-    var_Ypre = np.diag(var_Ypre)
-    return mean_Ypre, var_Ypre
+def logdetX(X):
+    L = np.linalg.cholesky(X)
+    y = 2 * sum(np.log(np.diag(L)))
+    return y

bo_core/GaussProcess.py
@@ -14,13 +14,12 @@ class GP:
         # d by N array
         X = np.asarray(X, dtype=float)
         y = np.asarray(y, dtype=float)
-        if len(X.shape) < 2:
+        if len(X.shape) < 2 or len(y) < 2:
             raise ValueError('the number of data points must be at least 2')
         # Training data sample size
         self.N = np.size(X, 0)
         # Demean train data
         self.X, self.meanX, self.stdX = demean(X)
-
         # Output data is a 1D numpy array
         y = np.asarray(y, dtype=float)
         self.y, self.meany = demean(y, std=0)
@@ -29,17 +28,17 @@ class GP:
         if self.N != n2:
             raise ValueError('the number of output and input training data samples must be equal')
 
-    # Compute marginal log-likelihood
+    # Compute the negative marginal log-likelihood
     def negloglik(self, par):
         par = np.squeeze(par)
         _, Ks, invKs = choleInvKs(par, self.X, covSE)
         # Compute negative log-likelihood Eq 2.30 of RW book
         negloglik = self.N / 2 * np.log(2 * np.pi)
         negloglik += 1 / 2 * np.dot(np.dot(self.y.T, invKs), self.y)
-        negloglik += 1 / 2 * np.log(np.linalg.det(Ks))
+        negloglik += 1 / 2 * logdetX(Ks)
         return negloglik
 
-    # method for computing the derivative of the negloglike
+    # Method for computing the derivative of the negloglike
     def der_negloglik(self, par):
         par = np.squeeze(par)
         K, Ks, invKs = choleInvKs(par, self.X, covSE)
@@ -52,34 +51,47 @@ class GP:
         der_temp = covSE(par, self.X, self.X, trainmode=1)
         der[0] = 1 / 2 * np.trace(np.dot(temp, der_temp))
         # Derivative w.r.t par[1]
-        der_temp = covSE(par, self.X, self.X, trainmode=2)
-        der[1] = 1 / 2 * np.trace(np.dot(temp, der_temp))
+        der_temp1 = covSE(par, self.X, self.X, trainmode=2)
+        der[1] = 1 / 2 * np.trace(np.dot(temp, der_temp1))
         # Derivative w.r.t par[2]
-        der_temp = 2 * par[2] * np.eye(self.N)
-        der[2] = 1 / 2 * np.trace(np.dot(temp, der_temp))
+        der_temp2 = 2 * par[2] * np.eye(self.N)
+        der[2] = 1 / 2 * np.trace(np.dot(temp, der_temp2))
         return der
 
+    # Fit gaussian process
     def fit(self, nstarts=10):
+        '''
+        nstarts = number of random starts
+        '''
         temp = np.zeros((nstarts, 3))
         fval = np.zeros(nstarts)
         for i in range(0, nstarts):
             par0 = np.random.randn(3)
-            res = spmin(self.negloglik, par0, method='L-BFGS-B', jac=self.der_negloglik, options={'gtol': 1e-6, 'disp': False})
+            res = spmin(self.negloglik, par0, method='L-BFGS-B', jac=self.der_negloglik, options={'gtol': 1e-4, 'disp': False})
             temp[i, :] = np.squeeze(res.x)
             fval[i] = np.squeeze(res.fun)
+        # Choose the start yielding the Max LL
         idx = np.argmin(fval)
         par_bar = temp[idx, :]
         return par_bar
 
     # Posterior prediction
-    def GP_predict(self, par_bar, Xpre):
+    def predict(self, par_bar, Xpre):
         # Testing data sample size
+        Xpre = np.asarray(Xpre)
         if len(Xpre.shape) == 1:
             Xpre = Xpre.reshape(1, -1)
         Npre = np.size(Xpre, 0)
-        # Demean test data
+        # standardize test data
         temp = np.tile(self.meanX, (Npre, 1))
         Xpre -= temp
         Xpre /= self.stdX
-        mean_Ypre, var_Ypre = Predict(par_bar, self.X, self.y, self.meany, Xpre, covSE)
+        _, _, invKs = choleInvKs(par_bar, self.X, covSE)
+        kpre1 = covSE(par_bar, self.X, Xpre)
+        # Eq2.25
+        mean_Ypre = np.dot(np.dot(kpre1.T, invKs), self.y)
+        temp = self.meany * np.ones(Npre)
+        mean_Ypre += temp
+        # Eq2.26
+        var_Ypre = par_bar[0]**2 - np.diag(np.dot(np.dot(kpre1.T, invKs), kpre1))
         return mean_Ypre, var_Ypre

test.py
@@ -1,32 +1,23 @@
+import pandas as pd
 import numpy as np
-from bo_core import *
 import matplotlib.pyplot as pl
+from bo_core import *
+df_train = pd.read_csv('sarcos_train.csv', header=None)
+df_test = pd.read_csv('sarcos_test.csv', header=None)
+# Training data
+xtrain = df_train.loc[:300, :20]
+ytrain = df_train.loc[:300, 21]
+# Testing data
+xtest = df_test.loc[0:149, :20]
+ytest = df_test.loc[0:149, 21]
+Nt = xtest.shape[0]
 
-
-def out(X):
-    return np.cos(X)
-
-
-N = 10
-n = 100
-s = 0.005
-x0 = np.linspace(-5, 5, N).reshape(N, 1)
-y0 = out(x0) + s * np.random.randn(N, 1)
-
-xtest = np.linspace(-5, 5, n).reshape(n, 1)
-ytest = out(xtest)
-gp = GP(x0, y0)
+gp = GP(xtrain, ytrain)
 par = gp.fit(nstarts=20)
-print(par)
-ypre, varypre = gp.GP_predict(par, xtest)
+ypre, varypre = gp.predict(par, xtest)
 ypre = np.squeeze(ypre)
-print(ypre)
-# BO = BayesOpt(x0, y0, ((-10, 10)))
 
-pl.plot(x0, y0, 'bs', ms=8)
-
-pl.gca().fill_between(np.squeeze(xtest), ypre - 2 * np.sqrt(varypre), ypre + 2 * np.sqrt(varypre), color="#dddddd")
-pl.plot(xtest, ypre, 'r--', lw=2)
-# # pl.axis([-5, 5, -3, 3])
-# # pl.title('Three samples from the GP posterior')
+pl.plot(np.arange(0, Nt), ytest, 'b-.', lw=2)
+pl.gca().fill_between(np.arange(0, Nt), ypre - 2 * np.sqrt(varypre), ypre + 2 * np.sqrt(varypre), color="#dddddd")
+pl.plot(np.arange(0, Nt), ypre, 'r--', lw=2)
 pl.show()